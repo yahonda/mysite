38bca0be65535d31b4e93f86eadbd2e9e79e2670
Yasuo Honda <yahonda@pingcap.com>
2024-07-19 13:27:27 +0900

    Add Post and Comment models

--- mysite/blogs/models.py
+++ mysite/blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
