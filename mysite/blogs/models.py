ea757637d4addd5bbb5e892ebb30af19cb9f6004
Yasuo Honda <yahonda@pingcap.com>
2024-07-22 13:58:14 +0900

    Add Author and Book models

--- mysite/blogs/models.py
+++ mysite/blogs/models.py
@@ -11,3 +11,18 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        tidb_auto_id_cache = 1
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        tidb_auto_id_cache = 1
