a2e27a06a942232a5bf465a1fb1875d09d54fae7
Yasuo Honda <yahonda@pingcap.com>
2024-07-22 14:36:00 +0900

    Revert "Revert "Use auto_random for post and comment""
    This reverts commit 037247af7320801b2068faafdc41f918338ebf0a.

--- mysite/blogs/models.py
+++ mysite/blogs/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django_tidb.fields import BigAutoRandomField
 
 # Create your models here.
 class Post(models.Model):
+    id = BigAutoRandomField(primary_key=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
+    id = BigAutoRandomField(primary_key=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
